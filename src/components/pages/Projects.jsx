7b244204be153cfbe526f2f4503566f60158f7ab
Carlos E. Osuna Izquierdo <cosunai@hotmail.com>
2023-10-04 14:49:18 -0600

    new project

--- src/components/pages/Projects.jsx
+++ src/components/pages/Projects.jsx
@@ -6,9 +6,13 @@ export const Projects = (props) => {
         <div className="projects">
             <h1 className="titulo">Projects</h1>
 
-            <Project name = {"Social Media REST Backend"} skill = {'JavaScript / CSS'} img = {'social.jpg'} link = {'https://github.com/OkamiXX/Social-Media-API-REST'}/>
-            <Project name = {"Book Finder"} skill = {'JavaScript / CSS'} img = {'2.png'} link = {'https://github.com/jeffrpar/book-review'}/>
-            <Project name = {"Tech Blog APP"} skill = {'JavaScript / CSS'} img = {'book.jpg'} link = {'https://github.com/OkamiXX/14-Tech-Blog-APP'}/>
+            <Project name={"Bundle Spares"} skill={'React / MongoDB / GraphQL'} img={
+                'https://github.com/jeffrpar/bundle-spares/blob/main/assets/bundle-spares-screenshot.JPG?raw=true'
+            } link={'https://github.com/jeffrpar/bundle-spares'} />
+            <Project name={"Social Media REST Backend"} skill={'JavaScript / CSS'} img={'social.jpg'} link={'https://github.com/OkamiXX/Social-Media-API-REST'} />
+            <Project name={"Book Finder"} skill={'JavaScript / CSS'} img={'book.png'} link={'https://github.com/jeffrpar/book-review'} />
+            <Project name={"Tech Blog APP"} skill={'JavaScript / CSS'} img={'2.jpg'} link={'https://github.com/OkamiXX/14-Tech-Blog-APP'} />
+
 
 
 
